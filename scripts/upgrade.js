30fb86c3d4efc745f7e54afd1d6d17a29923b2f8
stablecoinfactory <102005352+stablecoinfactory@users.noreply.github.com>
2022-03-20 21:54:41 -0700

    Update upgrade.js

--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -1,7 +1,7 @@
 const { ethers, upgrades } = require('hardhat')
 
 async function main() {
-  const CONTROLLER_ADDRESS = '' 
+  const CONTROLLER_ADDRESS = '0xF80a7ec392563A141BECfa6Ea108c3fE79619271' 
 
   const ControllerContract = await ethers.getContractFactory('Controller')
 
